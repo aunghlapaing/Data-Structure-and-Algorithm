744bcef358e595728e3495bd9786f0cead99383c
aunghlapaing <aunghlapaing8949@gmail.com>
2025-06-26 20:16:29 +0700

    bugFixed

--- src/main/java/com/programming/turing_dsa/search_test/SearchingAlgorithm.java
+++
@@ -1,6 +0,0 @@
-package com.programming.turing_dsa.search_test;
-
-public interface SearchingAlgorithm {
-	public int search(int arr[], int item);
-
-}
--- src/main/java/com/programming/turing_dsa/sorting_test/SortingAlgorithm.java
+++
@@ -1,5 +0,0 @@
-package com.programming.turing_dsa.sorting_test;
-
-public interface SortingAlgorithm {
-	public void sort(int [] arr);
-}
